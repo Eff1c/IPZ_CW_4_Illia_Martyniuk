67694c268ac6291931e658248cdb7c8f1640d570
eff1c <illia.martyniuk@busmarket.ua>
2024-03-14 15:00:44 +0200

    feat: Add navigation to MainActivity.kt

--- app/src/main/java/ua/edu/lntu/ipz_cw_4/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/ipz_cw_4/MainActivity.kt
@@ -3,44 +3,15 @@ package ua.edu.lntu.ipz_cw_4
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.enableEdgeToEdge
 import ua.edu.lntu.ipz_cw_4.ui.theme.IPZ_CW_4_Illia_MartyniukTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
-            IPZ_CW_4_Illia_MartyniukTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            IPZ_CW_4_Illia_MartyniukTheme { NavigationFun() }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    IPZ_CW_4_Illia_MartyniukTheme {
-        Greeting("Android")
     }
 }
