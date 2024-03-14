cdf81a7c595436e44819a9a4290796c10f83167b
eff1c <illia.martyniuk@busmarket.ua>
2024-03-14 15:14:27 +0200

    feat: Add navigation to task list column

--- app/src/main/java/ua/edu/lntu/ipz_cw_4/screens/TaskListScreen.kt
+++ app/src/main/java/ua/edu/lntu/ipz_cw_4/screens/TaskListScreen.kt
@@ -1,9 +1,8 @@
 package ua.edu.lntu.ipz_cw_4.screens
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -13,13 +12,13 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import ua.edu.lntu.ipz_cw_4.Router
 import ua.edu.lntu.ipz_cw_4.Task
 import ua.edu.lntu.ipz_cw_4.ui.theme.IPZ_CW_4_Illia_MartyniukTheme
 
@@ -41,13 +40,21 @@ fun TaskListScreen(modifier: Modifier = Modifier, navController: NavController) 
                     .padding(8.dp),
                 shape = RoundedCornerShape(8.dp),
                 colors = CardDefaults.cardColors(
-                    containerColor = if (it.isActive) Color.Red else Color.Green
+                    containerColor = it.getColor()
                 )
             ) {
-                Column {
+                Column(
+                    Modifier.clickable {
+                        navController.navigate(
+                            Router.TaskInfoScreen(
+                                it.name,
+                                it.description,
+                                it.isActive
+                            ).route
+                        )
+                    }
+                ) {
                     Text(text = it.name, style = TextStyle(fontSize = 34.sp))
-                    Spacer(modifier = Modifier.height(8.dp))
-                    Text(text = it.description)
                 }
             }
         }
@@ -56,7 +63,7 @@ fun TaskListScreen(modifier: Modifier = Modifier, navController: NavController) 
 
 @Preview
 @Composable
-fun TaskListScreen() {
+fun TaskListScreenPrev() {
     IPZ_CW_4_Illia_MartyniukTheme {
         TaskListScreen(navController = NavController(LocalContext.current))
     }
