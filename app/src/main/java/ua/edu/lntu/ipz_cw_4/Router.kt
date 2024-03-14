0395a17367b505f17bafae190ec1bfbeb2949181
eff1c <illia.martyniuk@busmarket.ua>
2024-03-14 15:08:44 +0200

    feat: Add TaskInfoScreen to Router

--- app/src/main/java/ua/edu/lntu/ipz_cw_4/Router.kt
+++ app/src/main/java/ua/edu/lntu/ipz_cw_4/Router.kt
@@ -2,4 +2,9 @@ package ua.edu.lntu.ipz_cw_4
 
 sealed class Router(val route:String){
     data object TaskListScreen : Router(route = "task_list_screen")
+
+    class TaskInfoScreen(name: String, description: String, isActive: Boolean):
+        Router(
+            route = "list_item_screen?name=$name&description=$description&isActive=$isActive"
+        )
 }
